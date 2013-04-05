98311a8c897a9dde579aa0a4880766ba57ed1740
Simon Brewin <sibrew@gmail.com>
2013-04-05 14:52:48 +0100

    added fb button added cheese int to main

---
+++ EgitTest/gen/com/siliform/android/egittest/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.siliform.android.egittest;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- EgitTest/src/com/siliform/android/egittest/MainActivity.java
+++ EgitTest/src/com/siliform/android/egittest/MainActivity.java
@@ -6,12 +6,13 @@ import android.view.Menu;
 
 public class MainActivity extends Activity
 {
-
+	public int cheese;
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_layout);
+		
 	}
 
 	@Override
